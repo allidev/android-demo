cdf61dbb9617f327de9a8ced4277f366154cee52
Xuesong Hu <xuesong@allienterprises.com>
2017-01-02 00:30:37 -0800

    copy armeabi to armeabi-v7a.

---
+++ examples/example_android/app/prebuilt/armeabi-v7a/include/ALLIVaultCoreP/unix/ALLIAppInit.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../alli_common.h"
+
+namespace boost
+{
+	namespace filesystem
+	{
+		class path;
+	}
+}
+
+namespace ALLIVaultCore
+{
+	namespace Helpers
+	{
+		class alli_mutex;
+		class ALLIAppInit
+		{
+		public:
+			static ALLIVAULTCOREP_API void appInitialize(const boost::filesystem::path &rootPath);
+			static ALLIVAULTCOREP_API void appTearDown();
+			static ALLIVAULTCOREP_API void increment_git_threads_counter();
+			static ALLIVAULTCOREP_API void decrement_git_threads_counter();
+
+		private:
+			static alli_mutex *git_mutex;
+			static unsigned int git_threads_counter;
+		};
+	}
+}
