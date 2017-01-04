d3b7ca4e50f82216b11bd166afbe10177b564678
Xuesong Hu <xuesong@allienterprises.com>
2017-01-03 18:12:39 -0800

    update a header file.

--- prebuilt/android/19/armeabi/include/ALLIVaultCoreP/ALLIUserP.h
+++ prebuilt/android/19/armeabi/include/ALLIVaultCoreP/ALLIUserP.h
@@ -22,6 +22,46 @@ namespace ALLIVaultCore
 		public:
 			std::string username;
 			/**
+			* Function pointer for plain repo.
+			*
+			* @param a boost filesystem path object
+			*/
+			void(*open_plain_repo_fn_ptr)(const boost::filesystem::path &);
+			/**
+			* Function pointer for encrypt repo.
+			*
+			* @param a boost filesystem path object
+			*/
+			void(*open_encrypt_repo_fn_ptr)(const boost::filesystem::path &);
+			/**
+			* Function pointer for plain folder.
+			*
+			* @param a boost filesystem path object
+			*/
+			void(*monitor_plain_folder_fn_ptr)(const boost::filesystem::path &);
+			/**
+			* Function pointer for mailbox registry. This is for Windows use only.
+			*/
+			void(*mb_registry_fn_ptr)();
+			/**
+			* Function pointer for mailbox plain repo.
+			*
+			* @param a boost filesystem path object
+			*/
+			void(*mb_open_plain_repo_fn_ptr)(const boost::filesystem::path &);
+			/**
+			* Function pointer for mailbox encrypt repo.
+			*
+			* @param a boost filesystem path object
+			*/
+			void(*mb_open_encrypt_repo_fn_ptr)(const boost::filesystem::path &);
+			/**
+			* Function pointer for mailbox plain folder.
+			*
+			* @param a boost filesystem path object
+			*/
+			void(*mb_monitor_plain_folder_fn_ptr)(const boost::filesystem::path &);
+			/**
 			 * Function pointer for network reachability
 			 *
 			 * @return a value from enum NetworkStatus
