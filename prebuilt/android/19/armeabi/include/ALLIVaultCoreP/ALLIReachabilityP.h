e93598a786c1efe14b58b4c465d66f560daff32a
Xuesong Hu <xuesong@allienterprises.com>
2017-01-03 16:27:18 -0800

    update a header file.

--- prebuilt/android/19/armeabi/include/ALLIVaultCoreP/ALLIReachabilityP.h
+++ prebuilt/android/19/armeabi/include/ALLIVaultCoreP/ALLIReachabilityP.h
@@ -1,3 +1,9 @@
+/*
+* Copyright (C) ALLI Enterprises, Inc. ALL rights reserved.
+*
+* This file is part of ALLIVault. For full terms see the included
+* COPYING file.
+*/
 #pragma once
 
 #include "alli_common.h"
@@ -7,12 +13,22 @@ namespace ALLIVaultCore
 {
 	namespace Engine
 	{
+		/**
+		* Class ALLIReachabilityP. It handles network connectability
+		* issue.
+		*/
 		class ALLIReachabilityP
 		{
 		public:
 			ALLIVAULTCOREP_API ALLIReachabilityP(void);
 			ALLIVAULTCOREP_API ~ALLIReachabilityP(void);
 			ALLIVAULTCOREP_API NetworkStatus currentReachabilityStatus();
+			/**
+			 * Static member function to check the network status to
+			 * the servers
+			 *
+			 * @return NetworkStatus indicating if the host is reachable
+			 */
 			static ALLIVAULTCOREP_API NetworkStatus currentReachabilityStatusS();
 			ALLIVAULTCOREP_API bool connectionRequired();
 		};
