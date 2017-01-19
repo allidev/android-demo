dd8cde9728d65b74b7fde204a46a8ce0e453bd3c
Xuesong Hu <xuesong@allienterprises.com>
2017-01-19 00:20:26 -0800

    add some header files.

---
+++ prebuilt/android/19/armeabi/include/ALLIVaultCoreP/ALLIEXTFolderP.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "ALLIFolderP.h"
+
+struct sqlite3_stmt;
+
+namespace ALLIVaultCore
+{
+	namespace Engine
+	{
+		class ALLIMonitorP;
+	}
+	class ALLIEXTFolderP :
+		public ALLIFolderP
+	{
+	public:
+		ALLIEXTFolderP();
+		ALLIEXTFolderP(const ALLIVaultCore::ALLIEXTFolderP &src);
+		~ALLIEXTFolderP();
+
+		void load_index_db();
+		/**
+		 * Get the list of files that are contained in the root folder.
+		 *
+		 * @return an unordered_map object of string and ALLIFolderIndex
+		 */
+		ALLIVAULTCOREP_API std::unordered_map<std::string, ALLIVaultCore::Engine::ALLIFolderIndex> getFolderContentList();
+
+	protected:
+		bool switching;
+		std::string serverRootFolder;
+		ALLIVaultCore::Engine::ALLIMonitorP *monitor;
+
+		virtual void load_index_db_ex();
+		bool setDataVersion(int ver);
+		bool isDataVersionSet(int ver);
+		int verctrl_query_callback(sqlite3_stmt *sqlstmt, int ver);
+		int updateDataVersion(int ver);
+	};
+}
+
