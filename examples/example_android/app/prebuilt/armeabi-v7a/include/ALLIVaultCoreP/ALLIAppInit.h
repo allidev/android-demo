b833ca74f4f2c91e4e9e0493915592360bc2228a
Xuesong Hu <xuesong@allienterprises.com>
2017-03-18 20:03:07 -0700

    update ALLIAppInit.h

--- examples/example_android/app/prebuilt/armeabi-v7a/include/ALLIVaultCoreP/ALLIAppInit.h
+++ examples/example_android/app/prebuilt/armeabi-v7a/include/ALLIVaultCoreP/ALLIAppInit.h
@@ -1,34 +1,10 @@
 #pragma once
 
-#include "../alli_common.h"
+#include "alli_common.h"
 
-namespace boost
-{
-	namespace filesystem
-	{
-		class path;
-	}
-}
+#ifdef ALLI_WIN32
+#include "win32\ALLIAppInit.h"
+#else
+#include "unix/ALLIAppInit.h"
+#endif
 
-namespace ALLIVaultCore
-{
-	namespace Helpers
-	{
-		class alli_mutex;
-		class ALLIAppInit
-		{
-		public:
-            static ALLIVAULTCOREP_API void appInitialize();
-			static ALLIVAULTCOREP_API void appInitialize(const
-            boost::filesystem::path &rootPath);
-			static ALLIVAULTCOREP_API void appInitialize(const boost::filesystem::path &rootPath, bool isTesting);
-			static ALLIVAULTCOREP_API void appTearDown();
-			static ALLIVAULTCOREP_API void increment_git_threads_counter();
-			static ALLIVAULTCOREP_API void decrement_git_threads_counter();
-
-		private:
-			static alli_mutex *git_mutex;
-			static unsigned int git_threads_counter;
-		};
-	}
-}
